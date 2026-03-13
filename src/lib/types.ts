5467e51fe23c701db857e0ab269b6d4023ca4a50
Paul Durbin <pauldurbin69@gmail.com>
2026-03-13 09:37:32 +0000

    chore: CI concurrency test - commit B (should cancel A)

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -9,3 +9,6 @@ export * from '@/models/types';
 // ci-validate: concurrency test commit 2
 
 // ci-validate: concurrency commit A
+// ci-validate: concurrency commit B git add src/lib/types.ts
+
+// ci-validate: concurrency commit B
